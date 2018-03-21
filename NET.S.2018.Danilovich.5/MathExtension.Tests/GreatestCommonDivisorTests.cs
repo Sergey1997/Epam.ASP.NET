3b8834a55fcebeac5b4b979015d006b1ff8f12bb
serdanilovich <serdanilovich@gmail.com>
2018-03-21 18:43:31 +0300

    Made GCD task

---
+++ NET.S.2018.Danilovich.5/MathExtension.Tests/GreatestCommonDivisorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MathExtension.Tests
+{
+    public class GreatestCommonDivisorTests
+    {
+        private static object[] sourceList =
+        {
+             new object[] { 4, new int[] { 32, 36, 44, 24 } },
+             new object[] { 50, new int[] { 4200, 2850, 4300, 2500 } },
+             new object[] { 25, new int[] { -300, 275, 2850, -25 } },
+             new object[] { 2, new int[] { 36, 22, 0,0 } },
+             new object[] { 5, new int[] { 55, 0, 20,40 } },
+             new object[] { 3, new int[] { 0, 0, 3, 6 } },
+             new object[] { 100, new int[] { 100 } },
+        };
+        [Test, TestCaseSource("sourceList")]
+        public static void EuclidsAlgorithmTest(int expected, params int[] numbers)
+        {
+            Assert.AreEqual(expected, GreatestCommonDivisor.EuclidsAlgorithm(numbers));
+        }
+
+        [Test, TestCaseSource("sourceList")]
+        public static void EuclidsBinaryAlgorithmTest(int expected, params int[] numbers)
+        {
+            Assert.AreEqual(expected, GreatestCommonDivisor.BinaryEuclideanAlgoritm(numbers));
+        }
+
+        [TestCase(new int[] { 0 })]
+        [TestCase(new int[] { 0, 0, 0 })]
+        [TestCase(null)]
+        public static void EuclidsAlgorithmTest_Numbers_ArgumentException(params int[] numbers)
+            => Assert.Throws<ArgumentException>(() => GreatestCommonDivisor.EuclidsAlgorithm(numbers));
+        
+        public static void EuclidsAlgorithmTest_Numbers_ArgumentNullException(int expected, params int[] numbers)
+            => Assert.Throws<ArgumentNullException>(() => GreatestCommonDivisor.EuclidsAlgorithm(numbers));
+    }
+}
---
+++ NET.S.2018.Danilovich.5/MathExtension/GreatestCommonDivisor.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace MathExtension
+{
+    public class GreatestCommonDivisor
+    {
+        /// <summary>   Euclids algorithm for int array. </summary>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are
+        ///                                             null. </exception>
+        /// <exception cref="ArgumentException">        Thrown when one or more arguments have
+        ///                                             unsupported or illegal values. </exception>
+        /// <param name="numbers">  A variable-length parameters list containing numbers. </param>
+        /// <returns>   Greatest Common Divisor of numbers like a integer value . </returns>
+        public static int EuclidsAlgorithm(params int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentNullException($"{nameof(numbers.Length)} must not be zero");
+            }
+
+            int greatestCommonDivisor = FindFirstNotZeroNumber(out int startIndex, numbers);
+
+            if (greatestCommonDivisor == 0)
+            {
+                throw new ArgumentException($"{nameof(numbers)} at least one of numbers must not be a zero");
+            }
+
+            for (int i = startIndex + 1; i < numbers.Length; i++)
+            {
+                while (greatestCommonDivisor != numbers[i] && numbers[i] != 0)
+                {
+                    if (numbers[i] < 0)
+                    {
+                        numbers[i] = -numbers[i];
+                    }
+
+                    if (greatestCommonDivisor > numbers[i])
+                    {
+                        greatestCommonDivisor -= numbers[i];
+                    }
+                    else
+                    {
+                        numbers[i] -= greatestCommonDivisor;
+                    }
+                }
+            }
+
+            return greatestCommonDivisor;
+        }
+
+        /// <summary>   Binary Euclidean algoritm for search of Greatest common divisor . </summary>
+        /// <exception cref="ArgumentException">    Thrown when one or more arguments have unsupported or
+        ///                                         illegal values. </exception>
+        /// <param name="numbers">  A variable-length parameters list containing numbers. </param>
+        /// <returns>   Greatest Common Divisor of numbers like a integer value . </returns>
+        public static int BinaryEuclideanAlgoritm(params int[] numbers)
+        {
+            int greatestCommonDivisor = FindFirstNotZeroNumber(out int startIndex, numbers);
+
+            if (greatestCommonDivisor == 0)
+            {
+                throw new ArgumentException($"{nameof(numbers)} at least one of numbers must not be a zero");
+            }
+
+            for (int i = startIndex + 1; i < numbers.Length; i++)
+            {
+                int k = 1;
+
+                if (numbers[i] == 0)
+                {
+                    numbers[i] = greatestCommonDivisor;
+                }
+
+                while (greatestCommonDivisor != 0 && numbers[i] != 0)
+                {
+                    if (numbers[i] < 0)
+                    {
+                        numbers[i] = -numbers[i];
+                    }
+
+                    while (greatestCommonDivisor % 2 == 0 && numbers[i] % 2 == 0)
+                    {
+                        greatestCommonDivisor /= 2;
+                        numbers[i] /= 2;
+                        k *= 2;
+                    }
+
+                    while (greatestCommonDivisor % 2 == 0)
+                    {
+                        greatestCommonDivisor /= 2;
+                    }
+
+                    while (numbers[i] % 2 == 0)
+                    {
+                        numbers[i] /= 2;
+                    }
+
+                    if (greatestCommonDivisor >= numbers[i])
+                    {
+                        greatestCommonDivisor -= numbers[i];
+                    }
+                    else
+                    {
+                        numbers[i] -= greatestCommonDivisor;
+                    }
+                }
+
+                greatestCommonDivisor = numbers[i] * k;
+            }
+
+            return greatestCommonDivisor;
+        }
+
+        /// <summary>  
+        /// Searches for the first not zero number in numbers. 
+        /// </summary>
+        /// <param name="index"> [out] Index for new point of entry. </param>
+        /// <param name="numbers">  A variable-length parameters list containing numbers. </param>
+        /// <returns>   The found first not zero number. </returns>
+        private static int FindFirstNotZeroNumber(out int index, params int[] numbers)
+        {
+            int greatestCommonDivisor = 0;
+            index = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    greatestCommonDivisor = numbers[i];
+                    index = i;
+                    break;
+                }
+            }
+
+            return Math.Abs(greatestCommonDivisor);
+        }
+    }
+}
