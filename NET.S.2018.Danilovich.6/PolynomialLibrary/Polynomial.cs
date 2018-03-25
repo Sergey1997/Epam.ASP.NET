7a794134caa1f4bbc36c999fcb56d3ab213d38b5
serdanilovich <serdanilovich@gmail.com>
2018-03-25 20:06:02 +0300

    added comments

--- NET.S.2018.Danilovich.6/PolynomialLibrary/Polynomial.cs
+++ NET.S.2018.Danilovich.6/PolynomialLibrary/Polynomial.cs
@@ -8,8 +8,15 @@ namespace PolynomialLibrary
 {
     public class Polynomial 
     {
+        /// <summary>   The coefficients. </summary>
         private double[] coefficients;
-
+        
+        /// <summary>   Constructor. </summary>
+        /// <exception cref="ArgumentNullException">        Thrown when one or more required arguments
+        ///                                                 are null. </exception>
+        /// <exception cref="ArgumentOutOfRangeException">  Thrown when one or more arguments are outside
+        ///                                                 the required range. </exception>
+        /// <param name="coefficients"> The coefficients of polinomial. </param>
         public Polynomial(double[] coefficients)
         {
             if (coefficients == null)
@@ -24,7 +31,9 @@ namespace PolynomialLibrary
 
             this.coefficients = coefficients;
         }
-
+        
+        /// <summary>   Gets the coefficients of polynomial. </summary>
+        /// <value> The coefficients. </value>
         public double[] Coefficients
         {
             get
@@ -33,6 +42,8 @@ namespace PolynomialLibrary
             }
         }
 
+        /// <summary>   Gets the degree of current polynomial. </summary>
+        /// <value> The degree. </value>
         public int Degree
         {
             get
@@ -45,6 +56,10 @@ namespace PolynomialLibrary
             }
         }
 
+        /// <summary>   Equality operator for polynomials. </summary>
+        /// <param name="lhs">  The left hand side. </param>
+        /// <param name="rhs">  The right hand side. </param>
+        /// <returns>   The result of the operation - bool. </returns>
         public static bool operator ==(Polynomial lhs, Polynomial rhs)
         {
             if (lhs.Coefficients.Length != rhs.Coefficients.Length)
@@ -64,12 +79,20 @@ namespace PolynomialLibrary
 
             return true;
         }
-
+        
+        /// <summary>   Inequality operator. </summary>
+        /// <param name="lhs">  The left hand side. </param>
+        /// <param name="rhs">  The right hand side. </param>
+        /// <returns>   The result of the operation. </returns>
         public static bool operator !=(Polynomial lhs, Polynomial rhs)
         {
             return lhs == rhs ? false : true;
         }
 
+        /// <summary>   Addition operator for two polynomials. </summary>
+        /// <param name="lhs">  The left hand side. </param>
+        /// <param name="rhs">  The right hand side. </param>
+        /// <returns>   The result of the operation (New polynomial). </returns>
         public static Polynomial operator +(Polynomial lhs, Polynomial rhs)
         {
             double[] coeff = new double[lhs.Coefficients.Length >= rhs.Coefficients.Length ? lhs.Coefficients.Length : rhs.Coefficients.Length];
@@ -82,6 +105,10 @@ namespace PolynomialLibrary
             return new Polynomial(coeff);
         }
 
+        /// <summary>   Subtraction operator for two polynomials. </summary>
+        /// <param name="lhs">  The left hand side. </param>
+        /// <param name="rhs">  The right hand side. </param>
+        /// <returns>   The result of the operation (New polynomial). </returns>
         public static Polynomial operator -(Polynomial lhs, Polynomial rhs)
         {
             double[] coeff = new double[lhs.Coefficients.Length >= rhs.Coefficients.Length ? lhs.Coefficients.Length : rhs.Coefficients.Length];
@@ -93,7 +120,12 @@ namespace PolynomialLibrary
 
             return new Polynomial(coeff);
         }
-
+        
+        /// <summary>   Multiplication operator. </summary>
+        /// <param name="lhs">  The left hand side. </param>
+        /// <param name="rhs">  The right hand side. </param>
+        ///
+        /// <returns>   The result of the operation. </returns>
         public static Polynomial operator *(Polynomial lhs, Polynomial rhs)
         {
             double[] coeff = new double[lhs.Coefficients.Length >= rhs.Coefficients.Length ? lhs.Coefficients.Length : rhs.Coefficients.Length];
