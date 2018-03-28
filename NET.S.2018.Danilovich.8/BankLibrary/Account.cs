16ace31659633404c91ff0647dc80ff3507e5c02
serdanilovich <serdanilovich@gmail.com>
2018-03-28 18:52:44 +0300

    Start of the task

---
+++ NET.S.2018.Danilovich.8/BankLibrary/Account.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public abstract class Account
+    {
+        private Man man;
+        private Gradation gradation;
+        private decimal balance;
+        public Account();
+
+        private uint Bonuses
+        {
+            get
+            {
+                return CostOfBalance * CostOfFiiling;
+            }
+        }
+        private uint CostOfBalance
+        {
+            get
+            {
+                switch (gradation)
+                {
+                    case Gradation.Base:
+                        return 1;
+                    case Gradation.Silver:
+                        return 3;
+                    case Gradation.Gold:
+                        return 5;
+                    case Gradation.Platinum:
+                        return 7;
+                }
+
+                throw new ArgumentException(nameof(gradation));
+            }
+        }
+        private uint CostOfFiiling
+        {
+            get
+            {
+                switch (gradation)
+                {
+                    case Gradation.Base:
+                        return 2;
+                    case Gradation.Silver:
+                        return 4;
+                    case Gradation.Gold:
+                        return 5;
+                    case Gradation.Platinum:
+                        return 7;
+                }
+
+                throw new ArgumentException(nameof(gradation));
+            }
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    enum Gradation
+    {
+        Base = 1,
+        Silver,
+        Gold,
+        Platinum
+    }
+}
---
+++ NET.S.2018.Danilovich.8/BankLibrary/Man.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class Man
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public virtual string GetShortName()
+        {
+            return $"{Surname} {Name[0]}.{Lastname[0]}.";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Man man = (Man)obj;
+            if ((Name == man.Name) || man.Name.Equals(Name))
+            {
+                if ((Surname == man.Surname) || man.Surname.Equals(Surname))
+                {
+                    if ((Lastname == man.Lastname) || man.Lastname.Equals(Lastname))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
