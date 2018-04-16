80b3f30b38be01469b41b2d6fed962ef75298359
serdanilovich <serdanilovich@gmail.com>
2018-04-16 23:32:19 +0300

    Binary Tree. To be continued.

---
+++ NET.S.2018.Danilovich.16/BinarySearchTreeLogic/BinarySearchTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeLogic
+{
+    class BinarySearchTree<T>
+    {
+        #region Fields
+        private TreeNode<T> node;
+        private IComparer<T> comparer;
+        private int size;
+        #endregion
+
+        #region Constructors
+        public BinarySearchTree()
+        {
+            node = null;
+            size = 0;
+        }
+        public BinarySearchTree(IComparer<T> comparer)
+        {
+            this.comparer = comparer ?? throw new ArgumentNullException($"{(nameof(comparer))} must not be a null");
+        }
+        #endregion
+        #region Properties
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            private set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"{(nameof(size))} cant be a less than zero");
+                }
+            }
+        }
+        #endregion
+        #region Methods
+        public void AddNode(T node)
+        {
+            if(this.node != null)
+            {
+                if(comparer == null)
+                {
+                    /// to be continued
+                }
+            }
+            else
+            {
+                this.node = new TreeNode<T>(node);
+            }
+        }
+        
+        #endregion
+
+    }
+}
---
+++ NET.S.2018.Danilovich.16/BinarySearchTreeLogic/TreeNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeLogic
+{
+    public class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+        public TreeNode()
+        {
+            Data = default(T);
+            Left = null;
+            Right = null;
+        }
+
+        public TreeNode(T data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
