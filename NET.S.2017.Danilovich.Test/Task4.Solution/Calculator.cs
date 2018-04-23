ec2cdb048c461ffb8a95f332e12c30b21991d3ca
serdanilovich <serdanilovich@gmail.com>
2018-04-23 11:49:36 +0300

    Task 4

---
+++ NET.S.2017.Danilovich.Test/Task4.Solution/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Solution
+{
+    class Calculator
+    {
+        public double CalculateAvarage(List<double> values, ICalculator calculator) => calculator.CalculateAverage(values);
+    }
+    public class MeanCalculator : ICalculator
+    {
+        public double CalculateAverage(List<double> values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException($"{(nameof(values))} musnt be a null");
+            }
+
+            return values.Sum() / values.Count;
+        }
+    }
+    public class MedianCalculator : ICalculator
+    {
+        public double CalculateAverage(List<double> values)
+        {
+            var sortedValues = values.OrderBy(x => x).ToList();
+
+                    int n = sortedValues.Count;
+
+                    if (n % 2 == 1)
+                    {
+                        return sortedValues[(n - 1) / 2];
+                    }
+
+                    return (sortedValues[sortedValues.Count / 2 - 1] + sortedValues[n / 2]) / 2;
+        }
+    }
+}
