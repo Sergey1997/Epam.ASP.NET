4f723f59a3ff8e5b33437f217cf9c7d457e199fc
serdanilovich <serdanilovich@gmail.com>
2018-05-10 18:44:17 +0300

    add readme

---
+++ NET.S.2018.Danilovich.16/MatrixLogic.Tests/MatrixMoqTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace MatrixLogic.Tests
+{
+    public class MatrixMoqTest
+    {
+        public void IndexViewModelNotNull()
+        {
+            SquareMatrix<double> square = new SquareMatrix<double>(3);
+            Calculater<double> visitor = new Calculater<double>(square);
+
+            Mock<SquareMatrix<double>> mock = new Mock<SquareMatrix<double>>();
+            mock.Setup(m => m.Accept(visitor));
+
+            DiagonalMatrix<double> matrix = new DiagonalMatrix<double>(mock.Object.Size);
+
+        }
+
+    }
+}
